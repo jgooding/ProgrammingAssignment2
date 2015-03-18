dd1ef436fda85f45d7596b1812bae3e7f2c45486
J Gooding <jgooding13@gmail.com>
2015-03-18 16:36:25 +0800

    Update to comments and test case output to show cache results

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,5 @@
-## Put comments here that give an overall description of what your
-## functions do
-
 ## makeCacheMatrix: Returns a list of functions that:
-## set the value of the matrix
+## Set the value of the matrix
 ## Get the value of the matrix
 ## Set the value of the matrix inverse
 ## Get the value of the matrix inverse
@@ -37,7 +34,7 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Otherwise solves and caches the matrix inverse
 
 cacheSolve <- function(x, ...) {
-  ## call the getinv() function 
+  ## call the getinv() function to get the inverse cache object
   store_inv <-x$getinv()
   
   ## Check value to see if inverse is already cached and 
@@ -53,20 +50,57 @@ cacheSolve <- function(x, ...) {
   ## create the inverse using solve() function
   store_inv <- solve(data_matrix, ...)
   
-  ## call setinv() function to store the inverse
+  ## call setinv() function to cache the inverse
   x$setinv(store_inv)
   
   ## Return solved inverse
   store_inv
 }
-## :Test case:
+## :Test case: This is the test script executed to validate
+## Define a matrix
 ## > x <- rbind((1:2),(3:4))
+## call makeCacheMatrix function with matrix as arguement
 ## > tst <- makeCacheMatrix(x)
+## Check object returns the four functions
+## > tst
+## $set
+## function (y) 
+## {
+##   x <<- y
+##   store_inv <<- NULL
+## }
+## <environment: 0x11080d88>
+##   
+##   $get
+## function () 
+##   x
+## <environment: 0x11080d88>
+##   
+##   $setinv
+## function (solved_inv) 
+##   store_inv <<- solved_inv
+## <environment: 0x11080d88>
+##   
+##   $getinv
+## function () 
+##   store_inv
+## <environment: 0x11080d88>
+## 
+## Check the object get function returns the Matrix as passed
 ## > tst$get()
 ##      [,1] [,2]
 ## [1,]    1    2
 ## [2,]    3    4
+## Call the cacheSolve function the first time
 ## > cacheSolve(tst)
+##      [,1] [,2]
+## [1,] -2.0  1.0
+## [2,]  1.5 -0.5
+##
+## Call the cacheSolve function again
+## Shows inverse came from cache second time round
+## > cacheSolve(tst)
+## getting cached data
 ##      [,1] [,2]
 ## [1,] -2.0  1.0
 ## [2,]  1.5 -0.5
